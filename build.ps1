a6348cb6428807ebf5aec01b70b4bace40da4f2f
Haralampi <haralampi@sofmon.com>
2016-12-26 16:45:00 +0100

    Build script for windows

---
+++ build.ps1
@@ -0,0 +1,17 @@
+Write-Host "=== Start building dart applicaiton === "
+cd ./editor; pub get; cd ..
+dart2js ./editor/editor.dart -m -o ./editor/editor.js
+If (Test-Path ./editor/editor.go) {
+    Remove-Item ./editor/editor.go 
+}
+$editorJS = Get-Content ./editor/editor.js
+"package editor" | Out-File ./editor/editor.go -Encoding UTF8 
+"const EditorJSCode = ``" | Out-File ./editor/editor.go -Encoding UTF8 -Append
+$jsContent = Get-Content ./editor/editor.js
+$jsContent = $jsContent.Replace("``","``+""``""+``")  #fix bug in golang with long strings
+$jsContent | Out-File ./editor/editor.go -Encoding UTF8 -Append
+"``" | Out-File ./editor/editor.go -Encoding UTF8 -Append
+Write-Host "=== Start building go applicaiton === "
+go get github.com/russross/blackfriday
+go get golang.org/x/net/html
+go build
